0066d6d841148f9fef06e37e30af440e2b8d44ea
GiovaniMurakami <113717151+GiovaniMurakami@users.noreply.github.com>
2023-06-18 17:08:49 +0000

    feat - added comentários para melhor leitura

--- main.c
+++ main.c
@@ -15,11 +15,13 @@ float a, b, k;
 int escolha;
 
 void integrate1X(float a, float b) {
+  //caso o intervalo de a e b contenha 0, integral não converge
   if (a * b <= 0) {
     printf("Integral não converge nesses valores");
     return;
   }
-  if (a < 0 && b < 0) { //módulo de A e B
+  //módulo de A e B
+  if (a < 0 && b < 0) { 
     a *= (-1);
     b *= (-1);
   }
@@ -28,30 +30,47 @@ void integrate1X(float a, float b) {
 
 void integrateXK(float k, float a, float b) {
   int aux = k;
-  if (k != aux && k > 0 && a < 0 || b < 0) {
-    printf("Integral não definida com k positivo e decimal e x < 0");
-    return;
+  //Caso k seja decimal e positivo
+  //e x negativo
+  //integral indefinida nesses valores
+  if (k != aux && 
+      k > 0 && 
+      a < 0 || 
+      b < 0) {
+        printf("Integral não definida com k positivo e decimal e x < 0");
+        return;
   }
-  if (k != aux && a <= 0 || b <= 0) {
-    printf("Integral não definida com k negativo e decimal e x <= 0");
-    return;
+  //Caso k seja decimal e negativo
+  //e x negativo ou igual a zero
+  //integral indefinida nesses valores
+  if (k != aux && 
+      k < 0 && 
+      a <= 0 || 
+      b <= 0) {
+        printf("Integral não definida com k negativo e decimal e x <= 0");
+        return;
   }
-  if (k == -1) { //caso k = -1, 1/x, devido a isso é necessário calcular log (b) - log(a);
-    //integrate1X(a, b);
-
+  //caso k = -1, necessário cálcular integral de 1/x
+  if (k == -1) {
     printf("Integral não definida com k = -1!");
-    
     return;
   }
+  //caso k negativo e o intervalo de a e b contenha 0, integral não converge
   if (k < 0 && a * b <= 0) {
     printf("Integral não converge neses valores");
     return;
   }
-  if (a < 0 && b < 0 && k < 0) {
-    a = 1/-((-k - 1) * (pow(a, -k - 1))); // x^-3 == 1/-(2x^2), devido a isso -k, o valor se torna positivo, quero esse valor -1
-    b = 1/-((-k - 1) * (pow(b, -k - 1)));  
-    printf("I = %f",  b-a);
-    return;
+  //caso k < 0 e diferente de -1, 
+  //necessário calcular quando k < 0, 1/(-k-1) * x ^(-k-1) 
+  //tanto no momento 'a' quanto no momento 'b'
+  //o resultado da integral se dá por b-a
+  if (a < 0 && 
+      b < 0 && 
+      k < 0) {
+        a = 1/-((-k - 1) * (pow(a, -k - 1)));
+        b = 1/-((-k - 1) * (pow(b, -k - 1)));  
+        printf("I = %f",  b-a);
+        return;
   }
     printf("I = %f", (pow(b, (k+1))/(k+1)) - (pow(a, (k+1))/(k+1)));
 };
@@ -60,7 +79,7 @@ int main() {
   do {
     printf("\n");
     printf("==================================================================\n");
-    printf("INTEGRAL DEFINIDA DE f(x) DE a ATE b\n");
+    printf("INTEGRAL DEFINIDA DE f(x) DE a até b\n");
     printf("Selecione uma das opções: 1 a 3\n");
     printf("==================================================================\n");
     printf("1.Integral de f(x) = x^k\n");
@@ -70,7 +89,6 @@ int main() {
       printf("Entrada inválida. Por favor, digite um valor numérico: \n");
       while (getchar() != '\n');
     }
-
     switch (escolha) {
     case 1:
       printf("Informe o valor de K\n");
